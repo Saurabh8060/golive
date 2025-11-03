ae00210c7db990f4df37f3eddc438e00a16f3f62
Saurabh Manchanda <saurabhm8060@gmail.com>
2025-11-03 19:54:11 +0530

    database-context

--- app/components/navigation/navigationBar.tsx
+++ app/components/navigation/navigationBar.tsx
@@ -1,17 +1,28 @@
-import { SignedIn, SignedOut, SignIn, SignInButton, SignOutButton } from '@clerk/nextjs'
 import React from 'react'
-
+import Image from 'next/image';
+import Link from 'next/link';
+import golivehubPurple  from '@/app/golivehub-purple.svg';
+import { Button } from '../button/button';
+import { EllipsisVertical } from '../icons';
+import SearchBar from './searchBar';
+import TrailingItems from './trailingItems';
 const NavigationBar = () => {
   return (
     <header>
         <nav className = 'w-full flex text-black items-center justify-between bg-white p-2 border-b border-slate-300'>
-            <SignedOut>
-                <SignInButton>
-                </SignInButton>
-            </SignedOut>
-             <SignedIn>
-                <SignOutButton />
-             </SignedIn>
+          <div className = 'flex items-center gap-6'>
+            <Link href = {'/'}>
+              <Image src = {golivehubPurple} alt='logo' width={32} height={32} />
+            </Link>
+            <Link href = {'browse'} className = 'text-black font-bold'>
+              Browse
+            </Link>
+            <Button variant = {'icon'}>
+              <EllipsisVertical />
+            </Button>
+            </div>
+            <SearchBar />
+            <TrailingItems />
         </nav>
     </header>
   )
