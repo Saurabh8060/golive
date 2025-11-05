ab3f91cd1af2c6c8462e331d0126074aaa876446
Saurabh Manchanda <saurabhm8060@gmail.com>
2025-11-06 02:51:19 +0530

    bugs-fixed

--- app/components/navigation/navigationBar.tsx
+++ app/components/navigation/navigationBar.tsx
@@ -1,31 +1,32 @@
-import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
-import golivehubPurple  from '@/app/golivehub-purple.svg';
-import { Button } from '../button/button';
+
+import goLiveHubPurple from '@/app/golivehub-purple.svg';
 import { EllipsisVertical } from '../icons';
 import SearchBar from './searchBar';
 import TrailingItems from './trailingItems';
-const NavigationBar = () => {
+import { Button } from '../button/button';
+
+export default function NavigationBar() {
   return (
     <header>
-        <nav className = 'w-full flex text-black items-center justify-between bg-white p-2 border-b border-slate-300'>
-          <div className = 'flex items-center gap-6'>
-            <Link href = {'/'}>
-              <Image src = {golivehubPurple} alt='logo' width={32} height={32} />
-            </Link>
-            <Link href = {'browse'} className = 'text-black font-bold'>
-              Browse
-            </Link>
-            <Button variant = {'icon'}>
-              <EllipsisVertical />
-            </Button>
-            </div>
-            <SearchBar />
-            <TrailingItems />
-        </nav>
-    </header>
-  )
-}
+      <nav className='w-full flex text-black items-center justify-between bg-white p-2 border-b border-slate-300'>
+        <div className='flex items-center gap-6'>
+          <Link href={'/'}>
+            <Image src={goLiveHubPurple} alt='logo' width={32} height={32} />
+          </Link>
+          <Link href={'/browse'} className='text-black font-bold'>
+            Browse
+          </Link>
+          <Button variant={'icon'}>
+            <EllipsisVertical />
+          </Button>
+        </div>
 
-export default NavigationBar
+        <SearchBar />
+
+        <TrailingItems />
+      </nav>
+    </header>
+  );
+}
